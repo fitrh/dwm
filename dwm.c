dc1402a188cf51b9ddf7f56bc4d013d03f754360
fitrh <fitrahm17h@student.unhas.ac.id>
2021-03-29 12:49:40 +0800

    apply inplacerotate patch with follow focus.
    the original patch, rotate client but the focus is unchanged,
    this modification always follow the focused client.

--- dwm.c
+++ dwm.c
@@ -224,6 +224,8 @@ static void incrivgaps(const Arg *arg);
 static void incrogaps(const Arg *arg);
 static void incrohgaps(const Arg *arg);
 static void incrovgaps(const Arg *arg);
+static void inplacerotate(const Arg *arg);
+static void insertclient(Client *item, Client *insertItem, int after);
 static void keypress(XEvent *e);
 static void killclient(const Arg *arg);
 static void manage(Window w, XWindowAttributes *wa);
@@ -1630,6 +1632,71 @@ incrovgaps(const Arg *arg)
 		selmon->gappih,
 		selmon->gappiv
 	);
+}
+
+void
+inplacerotate(const Arg *arg)
+{
+	if(!selmon->sel || (selmon->sel->isfloating && !arg->f)) return;
+
+	unsigned int selidx = 0, i = 0;
+	Client *c = NULL;
+        Client *shead = NULL, *stail = NULL;
+        Client *mhead = NULL, *mtail = NULL;
+
+	/* Determine positionings for insertclient */
+	for (c = selmon->clients; c; c = c->next) {
+		if (ISVISIBLE(c) && !(c->isfloating)) {
+		        if (selmon->sel == c)
+                                selidx = i;
+                        if (i == selmon->nmaster - 1)
+                                mtail = c;
+                        if (i == selmon->nmaster)
+                                shead = c;
+                        if (mhead == NULL)
+                                mhead = c;
+                        stail = c;
+                        i++;
+		}
+	}
+
+	/* All clients rotate */
+	if (arg->i == 2)
+                insertclient(selmon->clients, stail, 0);
+	if (arg->i == -2)
+                insertclient(stail, selmon->clients, 1);
+	/* Stack xor master rotate */
+	if (arg->i == -1 && selidx >= selmon->nmaster)
+                insertclient(stail, shead, 1);
+	if (arg->i == 1 && selidx >= selmon->nmaster)
+                insertclient(shead, stail, 0);
+	if (arg->i == -1 && selidx < selmon->nmaster)
+                insertclient(mtail, mhead, 1);
+	if (arg->i == 1 && selidx < selmon->nmaster)
+                insertclient(mhead, mtail, 0);
+
+	arrange(selmon);
+}
+
+void
+insertclient(Client *item, Client *insertItem, int after) {
+	Client *c;
+	if (item == NULL || insertItem == NULL || item == insertItem)
+                return;
+	detach(insertItem);
+	if (!after && selmon->clients == item) {
+		attach(insertItem);
+		return;
+	}
+	if (after) {
+		c = item;
+	} else {
+		for (c = selmon->clients; c; c = c->next) {
+                        if (c->next == item) break;
+                }
+	}
+	insertItem->next = c->next;
+	c->next = insertItem;
 }
 
 #ifdef XINERAMA

77d4eb6ad438ba3dbec7fa8abeb776779d8ba3f0
fitrh <fitrahm17h@student.unhas.ac.id>
2021-03-31 19:30:33 +0800

    cleanup spacing and allignment

--- config.def.h
+++ config.def.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 8;        /* snap pixel */
+static const unsigned int borderpx       = 1;  /* border pixel of windows */
+static const unsigned int snap           = 8;  /* snap pixel */
 /* vanitygaps */
-static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 8;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 8;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappih         = 8;  /* horiz inner gap between windows */
+static const unsigned int gappiv         = 8;  /* vert inner gap between windows */
+static const unsigned int gappoh         = 8;  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = 8;  /* vert outer gap between windows and screen edge */
+static       int smartgaps               = 0;  /* 1 means no outer gap when there is only one window */
 /* bar */
 static const unsigned int barheight      = 29; /* 0 means default height*/
-static const unsigned int centertitle    = 1;  /* 0 means left title */
+static const unsigned int centertitle    = 0;  /* 0 means left title */
 static const unsigned int colorfultitle  = 1;  /* 0 means title use SchemeTitle and SchemeTitleFloat */
 static const unsigned int colorfultag    = 1;  /* 0 means use SchemeSel for selected tag */
 static const unsigned int showvacanttags = 0;  /* 0 mean hide vacant tags */
@@ -20,29 +20,29 @@ static const int vertpad                 = 8;  /* vertical padding of bar */
 static const int sidepad                 = 8;  /* horizontal padding of bar */
 static const int showbar                 = 1;  /* 0 means no bar */
 static const int topbar                  = 1;  /* 0 means bottom bar */
-
-static const char *fonts[]          = { "SF Mono:style=Medium:size=10" };
-static const char dmenufont[]       = "SF Mono:style=Medium:size=10";
-static const char col_bg[]          = "#1a1b26";
-static const char col_dark[]        = "#16161E";
-static const char col_fg[]          = "#a9b1d6";
-static const char col_black[]       = "#32344a";
-static const char col_red[]         = "#f7768e";
-static const char col_green[]       = "#9ece6a";
-static const char col_yellow[]      = "#e0af68";
-static const char col_blue[]        = "#7aa2f7";
-static const char col_magenta[]     = "#ad8ee6";
-static const char col_cyan[]        = "#449dab";
-static const char col_white[]       = "#787c99";
-static const char col_br_black[]    = "#444b6a";
-static const char col_br_red[]      = "#ff7a93";
-static const char col_br_green[]    = "#b9f27c";
-static const char col_br_yellow[]   = "#ff9e64";
-static const char col_br_blue[]     = "#7da6ff";
-static const char col_br_magenta[]  = "#bb9af7";
-static const char col_br_cyan[]     = "#0db9d7";
-static const char col_br_white[]    = "#acb0d0";
-static const char *colors[][3]      = {
+/* font and color */
+static const char *fonts[]               = { "SF Mono:style=Medium:size=10" };
+static const char dmenufont[]            = "SF Mono:style=Medium:size=10";
+static const char col_bg[]               = "#1a1b26";
+static const char col_dark[]             = "#16161E";
+static const char col_fg[]               = "#a9b1d6";
+static const char col_black[]            = "#32344a";
+static const char col_red[]              = "#f7768e";
+static const char col_green[]            = "#9ece6a";
+static const char col_yellow[]           = "#e0af68";
+static const char col_blue[]             = "#7aa2f7";
+static const char col_magenta[]          = "#ad8ee6";
+static const char col_cyan[]             = "#449dab";
+static const char col_white[]            = "#787c99";
+static const char col_br_black[]         = "#444b6a";
+static const char col_br_red[]           = "#ff7a93";
+static const char col_br_green[]         = "#b9f27c";
+static const char col_br_yellow[]        = "#ff9e64";
+static const char col_br_blue[]          = "#7da6ff";
+static const char col_br_magenta[]       = "#bb9af7";
+static const char col_br_cyan[]          = "#0db9d7";
+static const char col_br_white[]         = "#acb0d0";
+static const char *colors[][3]           = {
 	/*                     fg               bg         border   */
 	[SchemeNorm]       = { col_fg,          col_bg,    col_black },   /* \x0b */
 	[SchemeSel]        = { col_blue,        col_dark,  col_br_black },/* \x0c */
@@ -93,6 +93,7 @@ static const char *tags[] = {
                                 "  ", "  ", "  ", "  " , "  ", 
                                 "  ", "  ", "  ", "  " 
 };
+
 static const char *tagsalt[] = {
                                 " 1 ", " 2 ", " 3 ", " 4 ", " 5 ",
                                 " 6 ", " 7 ", " 8 ", " 9 "
@@ -101,9 +102,11 @@ static const char *tagsalt[] = {
 static const int tagschemes[] = { SchemeTag1, SchemeTag2, SchemeTag3,
                                   SchemeTag4, SchemeTag5, SchemeTag6,
                                   SchemeTag7, SchemeTag8, SchemeTag9 };
+
 static const int titleschemes[] = { SchemeTitle1, SchemeTitle2, SchemeTitle3,
                                     SchemeTitle4, SchemeTitle5, SchemeTitle6,
                                     SchemeTitle7, SchemeTitle8, SchemeTitle9 };
+
 static const int swallowfloating    = 0; /* 1 means swallow floating windows by default */
 static int floatposgrid_x           = 5; /* float grid columns */
 static int floatposgrid_y           = 5; /* float grid rows */

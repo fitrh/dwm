d1d5887019db52058413bb81dcb4030adb37c3f6
fitrh <fitrahm17h@student.unhas.ac.id>
2021-06-21 10:52:42 +0800

    [ config ] update rules
        - add floatpos for discord updater to prevent it to be placed at the
          edge of the screen in multi monitor setup
        - adjust code style for rules

--- config.def.h
+++ config.def.h
@@ -142,18 +142,15 @@ static const Rule rules[] = {
          */
         RULE(.class = "Alacritty", .isterminal = 1, .noswallow = 1)
         RULE(.class = "codespace", .tags = 1 << 1)
-        RULE(.title = "Discord Updater", .tags = 1 << 6, .matchonce = 1)
+        RULE(.title = "Discord Updater", .tags = 1 << 6, .matchonce = 1,
+                .floatpos = "50% 50%")
         RULE(.class = "discord", .tags = 1 << 6, .floatpos = "0% 50% 70% 100%")
         RULE(.title = "Event Tester", .isfloating = 1, .noswallow = 1)
-        RULE(
-                .class = "File Manager",
-                .tags = 1 << 3, .isfloating = 1, .isterminal = 1
-        )
+        RULE(.class = "File Manager", .tags = 1 << 3, .isfloating = 1,
+                .isterminal = 1)
         RULE(.class = "firefoxdeveloperedition", .tags = 1 << 2, .noswallow = 1)
-        RULE(
-                .class = "firefoxdeveloperedition", .role = "Organizer",
-                .isfloating = 1
-        )
+        RULE(.class = "firefoxdeveloperedition", .role = "Organizer",
+                .isfloating = 1)
         RULE(.class = "Float Term", .isfloating = 1, .isterminal = 1)
         RULE(.class = "Gimp", .isfloating = 1)
         RULE(.class = "jetbrains-idea", .isfloating = 1, .noswallow = 1)
@@ -165,14 +162,10 @@ static const Rule rules[] = {
         RULE(.class = "Spotify", .tags = 1 << 5, .noswallow = 1)
         RULE(.class = "st", .isterminal = 1, .noswallow = 1)
         RULE(.class = "svkbd", .floatpos = "50% 100% 50% 50%")
-        RULE(
-                .class = "TelegramDesktop",
-                .tags = 1 << 6, .floatpos = "100% 50% 30% 100%"
-        )
-        RULE(
-                .class = "Tor Browser",
-                .tags = 1 << 2, .isfloating = 1, .noswallow = 1
-        )
+        RULE(.class = "TelegramDesktop", .tags = 1 << 6,
+                .floatpos = "100% 50% 30% 100%")
+        RULE(.class = "Tor Browser", .tags = 1 << 2, .isfloating = 1,
+                .noswallow = 1)
         RULE(.class = "zoom", .isfloating = 1)
         RULE(.wintype = WTYPE "UTILITY", .isfloating = 1)
 };

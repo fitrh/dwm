c02f869e3b416048b5fda9ca7fd6f9ed17feb6e7
fitrh <fitrahm17h@student.unhas.ac.id>
2022-02-04 19:42:58 +0800

    [config] Reorder rules

--- config.def.h
+++ config.def.h
@@ -167,9 +167,9 @@ static const Rule rules[] = {
 	RULE(.class = "st", .isterminal = 1, .noswallow = 1)
 	RULE(.class = "svkbd", .floatpos = "50% 100% 50% 50%")
 	RULE(.class = "Sxiv", .isfloating = 1)
-	RULE(.class = "Thunar", .tags = 1 << 3, .isfloating = 1)
 	RULE(.class = "TelegramDesktop", .tags = 1 << 6,
 		.floatpos = "100% 50% 30% 100%")
+	RULE(.class = "Thunar", .tags = 1 << 3, .isfloating = 1)
 	RULE(.class = "Tor Browser", .tags = 1 << 2, .isfloating = 1,
 		.noswallow = 1)
 	RULE(.class = "zoom", .isfloating = 1)
